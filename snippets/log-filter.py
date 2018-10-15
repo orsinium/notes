853453ca798c7e66b449fcc426b2922f2b035c9e
Gram <master_fess@mail.ru>
2018-10-15 13:33:12 +0300

    +log filter

---
+++ snippets/log-filter.py
@@ -0,0 +1,23 @@
+# built-in
+import logging
+
+
+class LevelFilter(logging.Filter):
+    """Filter log by min or max severity level.
+    """
+    def __init__(self, low=logging.DEBUG, high=logging.CRITICAL):
+        # Convert str level representation to level number.
+        # Example: "DEBUG" -> 10
+        if isinstance(low, str):
+            low = getattr(logging, low)
+        if isinstance(high, str):
+            high = getattr(logging, high)
+
+        self._low = low
+        self._high = high
+        super().__init__()
+
+    def filter(self, record):
+        if self._low <= record.levelno <= self._high:
+            return True
+        return False
